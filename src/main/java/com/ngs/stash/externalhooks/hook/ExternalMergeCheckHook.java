998693ba7d1d1107af86c2c926dae0fad17f5688
Dennis Ploeger <dennis.ploeger@kps.com>
2018-09-06 12:38:58 +0200

    Included checks, if the current version of a PR was already checked and used that to skip another check and commenting.

--- src/main/java/com/ngs/stash/externalhooks/hook/ExternalMergeCheckHook.java
+++ src/main/java/com/ngs/stash/externalhooks/hook/ExternalMergeCheckHook.java
@@ -1,12 +1,9 @@
 package com.ngs.stash.externalhooks.hook;
 
+import com.atlassian.activeobjects.external.ActiveObjects;
 import com.atlassian.bitbucket.comment.AddCommentRequest;
-import com.atlassian.bitbucket.comment.CommentSearchRequest;
 import com.atlassian.bitbucket.comment.CommentService;
-import com.atlassian.bitbucket.comment.CommentThread;
-import com.atlassian.bitbucket.comment.CommentThreadDiffAnchorState;
 import com.atlassian.bitbucket.hook.repository.*;
-import com.atlassian.bitbucket.property.PropertyMap;
 import com.atlassian.bitbucket.repository.*;
 import com.atlassian.bitbucket.setting.*;
 import com.atlassian.bitbucket.auth.*;
@@ -14,9 +11,11 @@ import com.atlassian.bitbucket.permission.*;
 import com.atlassian.bitbucket.server.*;
 import com.atlassian.bitbucket.util.*;
 
+import static java.util.logging.Level.FINER;
 import static java.util.logging.Level.SEVERE;
-import static java.util.logging.Level.INFO;
+import static java.util.logging.Level.FINEST;
 
+import java.util.Optional;
 import java.util.logging.Logger;
 
 import java.util.Collection;
@@ -29,6 +28,8 @@ import java.util.Set;
 import java.util.ArrayList;
 
 import com.atlassian.bitbucket.pull.*;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.ngs.stash.externalhooks.hook.data.PullRequestCheck;
 import com.ngs.stash.externalhooks.hook.helpers.*;
 // import com.google.common.base.Predicate;
 // import static com.google.common.base.Charsets.UTF_8;
@@ -45,10 +46,15 @@ import com.atlassian.upm.api.license.entity.PluginLicense;
 import com.atlassian.upm.api.util.Option;
 
 import com.atlassian.upm.api.license.PluginLicenseManager;
+import net.java.ao.DBParam;
+import net.java.ao.Query;
 import org.apache.commons.lang.BooleanUtils;
 
 import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import javax.inject.Named;
 
+@Named("ExternalMergeCheckHook")
 public class ExternalMergeCheckHook
     implements RepositoryMergeCheck, RepositorySettingsValidator
 {
@@ -62,7 +68,9 @@ public class ExternalMergeCheckHook
     private ApplicationPropertiesService properties;
     private PullRequestService pullRequestService;
     private CommentService commentService;
+    private ActiveObjects activeObjects;
 
+    @Inject
     public ExternalMergeCheckHook(
         AuthenticationContext authenticationContext,
         PermissionService permissions,
@@ -70,7 +78,8 @@ public class ExternalMergeCheckHook
         ApplicationPropertiesService properties,
         PluginLicenseManager pluginLicenseManager,
         PullRequestService pullRequestService,
-        CommentService commentService
+        CommentService commentService,
+        @ComponentImport ActiveObjects activeObjects
     )
     {
         this.authCtx = authenticationContext;
@@ -80,6 +89,7 @@ public class ExternalMergeCheckHook
         this.pluginLicenseManager = pluginLicenseManager;
         this.pullRequestService = pullRequestService;
         this.commentService = commentService;
+        this.activeObjects = activeObjects;
     }
 
     private static String cloneUrlFromRepository(
@@ -112,6 +122,36 @@ public class ExternalMergeCheckHook
     @Override
     public RepositoryHookResult preUpdate(PreRepositoryHookContext context, PullRequestMergeHookRequest request) {
         PullRequest pr = request.getPullRequest();
+
+        final Optional<PullRequestCheck> lastResult = this.getCheck(pr);
+        PullRequestCheck check;
+
+        if (lastResult.isPresent() && lastResult.get().getVersion() == pr.getVersion()) {
+            log.log(FINER, "Pull Request was already handled. Returning the last result.");
+            if (lastResult.get().getWasAccepted()) {
+                return RepositoryHookResult.accepted();
+            } else {
+                return RepositoryHookResult.rejected(
+                    lastResult.get().getLastExceptionSummary(),
+                    lastResult.get().getLastExceptionDetail()
+                );
+            }
+        } else {
+            if (lastResult.isPresent()) {
+                check = lastResult.get();
+                check.setVersion(pr.getVersion());
+                check.setWasHandled(false);
+            } else {
+                check = this.activeObjects.create(
+                    PullRequestCheck.class,
+                    new DBParam("PROJECT_ID", pr.getToRef().getRepository().getProject().getId()),
+                    new DBParam("REPOSITORY_ID", pr.getToRef().getRepository().getId()),
+                    new DBParam("PULL_REQUEST_ID", pr.getId()),
+                    new DBParam("VERSION", pr.getVersion())
+                );
+            }
+        }
+
         Repository repo = pr.getToRef().getRepository();
         Settings settings = context.getSettings();
 
@@ -194,14 +234,29 @@ public class ExternalMergeCheckHook
         }
 
         try {
-            return runExternalHooks(pb, refChanges, summaryMsg);
+            final RepositoryHookResult result = runExternalHooks(pb, refChanges, summaryMsg);
+            check.setWasAccepted(result.isAccepted());
+            if (result.isRejected()) {
+                check.setLastExceptionSummary(result.getVetoes().get(0).getSummaryMessage());
+                check.setLastExceptionDetail(result.getVetoes().get(0).getDetailedMessage());
+            }
+            check.save();
+            return result;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             String detailedMsg = "Interrupted";
+            check.setWasAccepted(false);
+            check.setLastExceptionDetail(detailedMsg);
+            check.setLastExceptionSummary(summaryMsg);
+            check.save();
             return RepositoryHookResult.rejected(summaryMsg, detailedMsg);
         } catch (IOException e) {
             log.log(SEVERE, "Error running " + exe + " in " + repoPath, e);
             String detailedMsg = "I/O Error";
+            check.setWasAccepted(false);
+            check.setLastExceptionDetail(detailedMsg);
+            check.setLastExceptionSummary(summaryMsg);
+            check.save();
             return RepositoryHookResult.rejected(summaryMsg, detailedMsg);
         }
     }
@@ -214,26 +269,20 @@ public class ExternalMergeCheckHook
         final PullRequest pr = request.getPullRequest();
         final Settings settings = context.getSettings();
 
-        final Page<PullRequestActivity> comments = this.pullRequestService.getActivities(
-            context.getRepository().getId(),
-            pr.getId(),
-            new PageRequestImpl(0, 10)
-        );
+        final Optional<PullRequestCheck> lastCheck = getCheck(pr);
 
-        final String refPrefix = String.format("`Version %s`\n\n", String.valueOf(pr.getVersion()));
-
-        if (comments.stream().anyMatch(commentThread -> {
-            if (!commentThread.getAction().equals(PullRequestAction.COMMENTED)) {
-                return false;
-            }
-
-            return ((PullRequestCommentActivity) commentThread).getComment().getText().startsWith(refPrefix);
-        })) {
-            // Reference has already been checked. Skip any further actions
+        if (!lastCheck.isPresent()) {
+            // this method was called without preUpdate first. This is unexpected.
             return;
         }
 
-        final StringBuffer comment = new StringBuffer(refPrefix);
+        if (lastCheck.get().getVersion() == pr.getVersion() && lastCheck.get().getWasHandled()) {
+            return;
+        }
+
+        log.log(FINEST, "Adding new comment");
+
+        final StringBuffer comment = new StringBuffer();
 
         if (result.isRejected()) {
 
@@ -247,6 +296,7 @@ public class ExternalMergeCheckHook
             );
 
             if (BooleanUtils.isTrue(settings.getBoolean("decline_pull_request_on_rejection"))) {
+                log.log(FINER, "Declining Pull Request as configured");
                 this.pullRequestService.decline(
                     new PullRequestDeclineRequest.Builder(
                         pr,
@@ -262,6 +312,33 @@ public class ExternalMergeCheckHook
                 ).build()
             );
         }
+
+        lastCheck.get().setWasHandled(true);
+        lastCheck.get().save();
+    }
+
+    private Optional<PullRequestCheck> getCheck(PullRequest pr) {
+
+        log.log(FINEST, "Fetching existing checks for this pull request");
+
+        final PullRequestCheck[] checks = this.activeObjects.find(
+            PullRequestCheck.class,
+            Query.select().where(
+                "PROJECT_ID = ? AND REPOSITORY_ID = ? AND PULL_REQUEST_ID = ?",
+                pr.getToRef().getRepository().getProject().getId(),
+                pr.getToRef().getRepository().getId(),
+                pr.getId()
+            )
+        );
+
+        if (checks.length > 1) {
+            log.log(SEVERE, "More than one checks exist. This is invalid. Returning.");
+            return Optional.empty();
+        } else if (checks.length == 1) {
+            return Optional.of(checks[0]);
+        }
+
+        return Optional.empty();
     }
 
     public ProcessBuilder createProcessBuilder(
