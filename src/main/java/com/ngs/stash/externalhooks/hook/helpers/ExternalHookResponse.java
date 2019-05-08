4c2a071a2f47d39b4dac8f39840da53c5a3b5480
Egor Kovetskiy <e.kovetskiy@gmail.com>
2019-05-08 10:58:31 +0300

    apply google-java-format

--- src/main/java/com/ngs/stash/externalhooks/hook/helpers/ExternalHookResponse.java
+++ src/main/java/com/ngs/stash/externalhooks/hook/helpers/ExternalHookResponse.java
@@ -1,28 +1,29 @@
 package com.ngs.stash.externalhooks.hook.helpers;
 
 import java.io.PrintWriter;
-import com.atlassian.bitbucket.hook.HookResponse;
 
 import javax.annotation.Nonnull;
 
+import com.atlassian.bitbucket.hook.HookResponse;
+
 public class ExternalHookResponse implements HookResponse {
-    PrintWriter outWriter;
-    PrintWriter errWriter;
+  PrintWriter outWriter;
+  PrintWriter errWriter;
 
-    public ExternalHookResponse(PrintWriter outWriter, PrintWriter errWriter) {
-        this.outWriter = outWriter;
-        this.errWriter = errWriter;
-    }
+  public ExternalHookResponse(PrintWriter outWriter, PrintWriter errWriter) {
+    this.outWriter = outWriter;
+    this.errWriter = errWriter;
+  }
 
-    @Nonnull
-    @Override
-    public PrintWriter out() {
-        return outWriter;
-    }
+  @Nonnull
+  @Override
+  public PrintWriter out() {
+    return outWriter;
+  }
 
-    @Nonnull
-    @Override
-    public PrintWriter err() {
-        return errWriter;
-    }
+  @Nonnull
+  @Override
+  public PrintWriter err() {
+    return errWriter;
+  }
 }
