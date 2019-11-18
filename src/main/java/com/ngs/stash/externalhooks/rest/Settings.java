3d7143901ed27e9e9b3181d30110902e9a8c5059
Egor Kovetskiy <e.kovetskiy@gmail.com>
2019-11-18 12:42:14 +0300

    add settings as xml document

--- src/main/java/com/ngs/stash/externalhooks/rest/Settings.java
+++ src/main/java/com/ngs/stash/externalhooks/rest/Settings.java
@@ -1,10 +1,23 @@
 package com.ngs.stash.externalhooks.rest;
 
+import java.util.List;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class Settings {
-  @XmlElement public String todo1;
-  @XmlElement public String todo2;
+  @XmlElement(name = "triggers")
+  public Triggers triggers;
+
+  public class Triggers {
+    @XmlElement(name = "pre_receive")
+    public List<String> PreReceive;
+
+    @XmlElement(name = "post_receive")
+    public List<String> PostReceive;
+
+    @XmlElement(name = "merge_check")
+    public List<String> MergeCheck;
+  }
 }
